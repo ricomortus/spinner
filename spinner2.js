331606a768a65cb26834d360f5d2efc3b725019d
Rico Chow <rico.ck.chow@gmail.com>
2023-10-12 12:00:54 -0400

    Add spinner2.js.

---
+++ spinner2.js
@@ -0,0 +1,14 @@
+const spinnerChar = ["|", "/" , "-", "\\", "|"];
+
+let delay = 100;
+for (const char of spinnerChar) {
+  setTimeout(() => {
+    process.stdout.write(`\r${char}    `);
+
+  }, delay);
+  delay += 200;
+}
+
+setTimeout(() => {
+  console.log();
+}, delay);
